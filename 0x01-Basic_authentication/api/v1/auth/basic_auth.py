a6eaefeecca652a0dd5cf81915196ccb22a7b6bf
StSama <albertezoula98@gmail.com>
2024-06-07 23:32:35 +0000

    task 11 try

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -69,20 +69,27 @@ class BasicAuth(Auth):
                 return user
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
-        overloads Auth and retrieves the User instance for a request
-        """
-        auth_value = self.authorization_header(request)
-        if not auth_value:
+        """ overloads Auth and retrieves the User instance for a request """
+        auth_header = self.authorization_header(request)
+
+        if not auth_header:
             return None
-        auth_base_part = self.extract_base64_authorization_header(auth_value)
-        if not auth_base_part:
+
+        encoded = self.extract_base64_authorization_header(auth_header)
+
+        if not encoded:
             return None
-        decoded_auth = self.decode_base64_authorization_header(auth_base_part)
-        if not decoded_auth:
+
+        decoded = self.decode_base64_authorization_header(encoded)
+
+        if not decoded:
             return None
-        email, pwd = self.extract_user_credentials(decoded_auth)
+
+        email, pwd = self.extract_user_credentials(decoded)
+
         if not email or not pwd:
             return None
-        user_object = self.user_object_from_credentials(email, pwd)
+
+        user = self.user_object_from_credentials(email, pwd)
+
         return user
