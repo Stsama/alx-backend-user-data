9c64d2384f40f5be4c0058d2c5d859d00f807266
StSama <albertezoula98@gmail.com>
2024-06-07 21:14:20 +0000

    adding comments to the task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -38,6 +38,9 @@ class BasicAuth(Auth):
 
     def extract_user_credentials(
             self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        eturns the user email and password from the Base64 decoded value
+        """
         if (decoded_base64_authorization_header is None or
                 not isinstance(decoded_base64_authorization_header, str) or
                 ":" not in decoded_base64_authorization_header):
