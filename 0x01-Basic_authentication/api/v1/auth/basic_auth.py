c4c2f0c824b679dbe6940f1db7201d144909bdc0
StSama <albertezoula98@gmail.com>
2024-06-08 01:14:37 +0000

    task 100 tested

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -49,7 +49,8 @@ class BasicAuth(Auth):
             return (None, None)
         string = str(decoded_base64_authorization_header)
         to_return = string.split(":")
-        return (to_return[0], to_return[1])
+        last = ":".join(to_return[1:])
+        return (to_return[0], last)
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
