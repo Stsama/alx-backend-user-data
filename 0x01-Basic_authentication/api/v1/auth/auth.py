6a7b6c89bf7654ba3a8c8297507d5fa68550d67a
StSama <albertezoula98@gmail.com>
2024-06-08 01:36:02 +0000

    task 101 done

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,13 +17,28 @@ class Auth:
         """
         verify if the authorization is reqquired
         """
-        check = path
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        l_path = len(path)
+        if l_path == 0:
             return True
-        if path[-1] != "/":
-            check += "/"
-        if path in excluded_paths or check in excluded_paths:
-            return False
+
+        slash_path = True if path[l_path - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
