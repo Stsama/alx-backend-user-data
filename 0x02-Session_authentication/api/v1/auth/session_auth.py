56f2292898b95c3cf654b3c2540eee60da8a743b
StSama <albertezoula98@gmail.com>
2024-06-09 02:03:19 +0000

    task 3 fixed

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,4 +30,4 @@ class SessionAuth(Auth):
         """
         if not session_id or not isinstance(session_id, str):
             return None
-        return SessionAuth.user_id_by_session_id.get(session_id)
+        return SessionAuth.user_id_by_session_id.get(session_id, None)
