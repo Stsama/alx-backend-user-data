bf0933e8d2d0d0f0c0d45198fae051c9f9dae25b
StSama <albertezoula98@gmail.com>
2024-06-09 01:54:09 +0000

    task 2 done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,10 +5,21 @@ Empty session
 
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     creating a new authentication mechanism
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id
+        """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = uuid.uuid4()
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
