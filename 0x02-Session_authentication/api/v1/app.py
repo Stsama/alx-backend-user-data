827cac6bc602c2a89fce55eb3f01ed6f0a619ba2
StSama <albertezoula98@gmail.com>
2024-06-09 02:41:27 +0000

    task 5 fixed

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,7 +32,10 @@ def before_request():
     authorization_path = ['/api/v1/status/',
                           '/api/v1/unauthorized/', '/api/v1/forbidden/',
                           '/api/v1/auth_session/login/']
+
     if auth and auth.require_auth(request.path, authorization_path):
+        if not auth.authorization_header(request):
+            abort(401)
         if not (auth.authorization_header(request) and
                 not auth.session_cookie(request)):
             abort(401)
