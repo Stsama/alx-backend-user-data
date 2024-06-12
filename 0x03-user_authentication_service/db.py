2ac40f3dbe937df9e8bb54f7022b712595b2ef76
StSama <albertezoula98@gmail.com>
2024-06-12 23:44:49 +0000

    task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
-"""DB module
+""" DB module
 """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from typing import TypeVar
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
+from typing import TypeVar
 
 
 class DB:
-    """DB class
+    """
+    DB class.
     """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
+    def __init__(self):
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Constructor.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
+    def _session(self):
+        """
+        _session.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -32,7 +37,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """
-        create user
+        add_user.
         """
         user = User(email=email, hashed_password=hashed_password)
         session = self._session
