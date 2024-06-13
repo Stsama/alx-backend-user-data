2cfb0e7adb6a519ce43871bda245b811f2efd5b3
StSama <albertezoula98@gmail.com>
2024-06-13 01:21:07 +0000

    task 6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/"), strict_slashes=False)
+def hello_world() -> str:
+    """
+    return a JSON payload of the form
+    """
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
