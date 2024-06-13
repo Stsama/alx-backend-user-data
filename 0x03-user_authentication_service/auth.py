0ea2fd46ba457c1d21178b570b02c113b6f58c8c
StSama <albertezoula98@gmail.com>
2024-06-13 01:10:07 +0000

    task 5 fixed and done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,11 +29,11 @@ class Auth:
         """
         self._db = DB()
 
-    def register_user(self, email=email: str, password=password: str) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """
         """
         try:
-            self.db.find_user_by(email=email)
+            self._db.find_user_by(email=email)
             raise ValueError(f'User {email} already exists')
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
